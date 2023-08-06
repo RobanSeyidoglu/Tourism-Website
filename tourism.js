d54c2dfef959eb5677d9c7d091180446f94467aa
RobanSeyidoglu <robanseyidoglu@gmail.com>
2023-08-06 13:31:13 +0300

    end header

---
+++ tourism.js
@@ -0,0 +1,27 @@
+// Some Functions In The Header
+let toggleMenu = document.querySelector("nav i");
+let menu = document.querySelector("header nav ul");
+let body = document.body;
+if (getComputedStyle(toggleMenu).display !== "none") {
+  toggleMenu.onclick = function () {
+    if (getComputedStyle(menu).display === "flex") {
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "flex";
+    }
+  };
+  document.addEventListener("click", function (event) {
+    let targetElement = event.target;
+    let isClickedInsideMenu = toggleMenu.contains(targetElement);
+    if (
+      !isClickedInsideMenu &&
+      !(getComputedStyle(toggleMenu).display === "none")
+    ) {
+      menu.style.display = "none";
+    }
+  });
+} else if (toggleMenu.style.display === "none") {
+  menu.style.display = "flex";
+}
+
+// End Header
